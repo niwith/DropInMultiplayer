4b1e8f049ba319d5b0566556d9d51f77f936463f
AaronEnglish <aaenglis@gmail.com>
2020-10-06 03:51:21 -0500

    CreateRelease post event working
    Script executes and builds a DropInMultiplayer.zip in the project directory

---
+++ DropInMultiplayer/CreateRelease1.bat
@@ -0,0 +1,10 @@
+﻿IF EXIST %~dp0DropInMultiplayer (rmdir /S /Y %~dp0DropInMultiplayer)
+IF EXIST %~dp0DropInMultiplayer.zip (del /F %~dp0DropInMultiplayer.zip)
+mkdir %~dp0DropInMultiplayer
+xcopy /Y %~dp0bin\Debug\DropinMultiplayer.dll %~dp0DropInMultiplayer
+xcopy /Y %~dp0bin\Debug\manifest.json %~dp0DropInMultiplayer
+xcopy /Y %~dp0bin\Debug\icon.png %~dp0DropInMultiplayer
+xcopy /Y %~dp0bin\Debug\README.md %~dp0DropInMultiplayer
+set src=%~dp0DropInMultiplayer
+powershell "Compress-Archive -Force %src% DropInMultiplayer.zip"
+exit 0
