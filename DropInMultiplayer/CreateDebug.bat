77745ca76963e212fd744a0a7e445786647e2646
Nicholas <7247591+niwith@users.noreply.github.com>
2021-02-22 20:10:46 +1000

    Added create debug helper

---
+++ DropInMultiplayer/CreateDebug.bat
@@ -0,0 +1,9 @@
+﻿IF EXIST %~dp0DropInMultiplayer (rmdir /S /Y %~dp0DebugOutput)
+IF EXIST %~dp0DropInMultiplayer.zip (del /F %~dp0DropInMultiplayer.zip)
+mkdir %~dp0DebugOutput
+xcopy /Y %~dp0bin\Debug\DropinMultiplayer.dll %~dp0DebugOutput
+xcopy /Y %~dp0bin\Debug\manifest.json %~dp0DebugOutput
+xcopy /Y %~dp0bin\Debug\icon.png %~dp0DebugOutput
+xcopy /Y %~dp0bin\Debug\README.md %~dp0DebugOutput
+powershell "Get-ChildItem .\DebugOutput\ | Compress-Archive -DestinationPath DebugOutput\DropInMultiplayer.zip -Update"
+exit 0 
