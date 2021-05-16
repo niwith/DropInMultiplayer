14f5a00c095c8475bc637797ff107e16d8aa290e
Nicholas <7247591+niwith@users.noreply.github.com>
2021-05-16 16:36:37 +1000

    Change create debug so it can drop straight into r2modmad

--- DropInMultiplayer/CreateDebug.bat
+++ DropInMultiplayer/CreateDebug.bat
@@ -2,7 +2,7 @@
 IF EXIST %~dp0DropInMultiplayer.zip (del /F %~dp0DropInMultiplayer.zip)
 mkdir %~dp0DebugOutput
 xcopy /Y %~dp0bin\Debug\DropinMultiplayer.dll %~dp0DebugOutput
-xcopy /Y %~dp0bin\Debug\manifest.json %~dp0DebugOutput
+xcopy /Y %~dp0bin\Debug\manifestv2.json %~dp0DebugOutput\manifest.json
 xcopy /Y %~dp0bin\Debug\icon.png %~dp0DebugOutput
 xcopy /Y %~dp0bin\Debug\README.md %~dp0DebugOutput
 powershell "Get-ChildItem .\DebugOutput\ | Compress-Archive -DestinationPath DebugOutput\DropInMultiplayer.zip -Update"
